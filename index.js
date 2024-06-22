e237a61e6c104381500e8a18d67b5ea86d10301a
Abel Koech <koechabel161@gmail.com>
2024-06-23 00:02:49 +0300

    arithmetic

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    return a + b;
+}
+console.log(add(300, 714));
+function subtract(a, b){
+    return a - b;
+}
+console.log(subtract(1, 33));
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(64.517, 1000));
+function divide(a, b){
+return a / b;
+}
+console.log(divide(106000000000, 100000000000));
+function increment(n){
+    return ++n;
+}
+console.log(increment(400, 17));
+function decrement(n){
+    return --n;
+}
+console.log(decrement)
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
